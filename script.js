0024e76744ff4e0d50a0d3b5eedb3b6a11023bc9
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-04-26 19:30:23 -0300

    improving logic in display. still needs work

---
+++ script.js
@@ -0,0 +1,90 @@
+//Calc functions
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function substract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    if(num2 !== 0) {
+        return num1 / num2
+    } else {
+        return "Syntax Err"
+    }
+}
+
+
+
+// Grab the buttons from the HTML
+
+const operators = document.querySelectorAll('.operator')
+const numbers = document.querySelectorAll('.operand')
+const decimal = document.querySelector('#decimal')
+const clear = document.querySelector('#clear')
+const equal = document.querySelector('#equal')
+
+const previousDisp = document.getElementById('previousDisp')
+const currentDisp = document.getElementById('currentDisp')
+const keyboard = document.getElementById('keyboard')
+
+let currentNum = ""
+let previousNum = ""
+let operator = ""
+let answer = 0
+
+function operate(operator, num1, num2) {
+    console.log("Esta es la cuenta: " + num1 + " "+ operator + " " + + num2)
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    switch (operator) {
+        case '+':
+            answer = add(num1, num2)
+            break;
+        case '-':
+            answer = substract(num1, num2)
+            break;
+        case '*':
+            answer = multiply(num1, num2)
+            break;
+        case '/':
+            answer = divide(num1, num2)
+            break;
+    }
+    return answer
+}
+// Display the number being typed
+numbers.forEach(btn => btn.addEventListener('click', function handleNum() {
+    console.log(btn.value)
+    if(currentNum.length < 8) {
+        currentNum += btn.innerText
+    }
+    currentDisp.innerText = `${currentNum}`
+}   
+))
+
+operators.forEach(op => op.addEventListener('click', e => {
+    handleOperator(e.target.textContent)
+}))
+
+function handleOperator(op) {
+    operator = op
+    previousNum = currentNum
+    previousDisp.innerText = `${previousNum} ${op}`
+    currentNum = ""
+    currentDisp.innerText = `${currentNum}`
+}
+
+equal.addEventListener('click', e => {
+    answer = operate(operator, previousNum, currentNum)
+    previousDisp.innerText = `${previousNum} ${operator} ${currentNum} =`
+    currentDisp.innerText = `${answer}`
+    currentNum = answer
+})
