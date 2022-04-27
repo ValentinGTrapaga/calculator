5b9fae3041ef15ed5aef2e0f47d1b9a622a9a551
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-04-27 00:49:34 -0300

    clear key programmed - still not able to calculate two ops in a row

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if(num2 !== 0) {
+    if (num2 !== 0) {
         return num1 / num2
     } else {
         return "Syntax Err"
@@ -27,9 +27,9 @@ function divide(num1, num2) {
 const operators = document.querySelectorAll('.operator')
 const numbers = document.querySelectorAll('.operand')
 const decimal = document.querySelector('#decimal')
-const clear = document.querySelector('#clear')
 const equal = document.querySelector('#equal')
 
+const clear = document.getElementById('clear')
 const previousDisp = document.getElementById('previousDisp')
 const currentDisp = document.getElementById('currentDisp')
 const keyboard = document.getElementById('keyboard')
@@ -39,8 +39,8 @@ let previousNum = ""
 let operator = ""
 let answer = 0
 
-function operate(operator, num1, num2) {
-    console.log("Esta es la cuenta: " + num1 + " "+ operator + " " + + num2)
+function operate(operator, num1, num2 = 0) {
+    console.log("Esta es la cuenta: " + num1 + " " + operator + " " + + num2)
     num1 = Number(num1)
     num2 = Number(num2)
 
@@ -61,13 +61,13 @@ function operate(operator, num1, num2) {
     return answer
 }
 // Display the number being typed
-numbers.forEach(btn => btn.addEventListener('click', function handleNum() {
+numbers.forEach(btn => btn.addEventListener('click', function typeNums() {
     console.log(btn.value)
-    if(currentNum.length < 8) {
+    if (currentNum.length < 8) {
         currentNum += btn.innerText
     }
     currentDisp.innerText = `${currentNum}`
-}   
+}
 ))
 
 operators.forEach(op => op.addEventListener('click', e => {
@@ -82,9 +82,27 @@ function handleOperator(op) {
     currentDisp.innerText = `${currentNum}`
 }
 
-equal.addEventListener('click', e => {
+function calculate() {
     answer = operate(operator, previousNum, currentNum)
     previousDisp.innerText = `${previousNum} ${operator} ${currentNum} =`
     currentDisp.innerText = `${answer}`
     currentNum = answer
+}
+
+function clearCalculator() {
+    previousNum = ""
+    currentNum = ""
+    answer = 0
+    previousDisp.innerText = ""
+    currentDisp.innerText = ""
+}
+
+//Need to check for two numbers
+equal.addEventListener('click', () => {
+    if (currentNum != "" && previousNum != "") {
+        calculate()
+    }
 })
+
+// Programming clear key
+clear.addEventListener('click', clearCalculator)
